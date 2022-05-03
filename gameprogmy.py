8178151a6f5c04306057dbd58c8d31fa3ed897d2
Dobby <nag_bit_8@mail.ru>
2022-05-03 19:23:09 +0300

    050322

---
+++ gameprogmy.py
@@ -0,0 +1,8 @@
+#cd C:\Users\nag_b\Desktop\atom-game
+#py -m pip install -U pygame --user
+#pip install jedi
+import pygame
+
+pygame.init()
+win = pygame.display.set_mode((500, 500))
+'''Рзрешение окна 500 на 500'''
